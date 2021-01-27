d7d3fb19648a738fd04e817f8cb2c016f3f8b8b4
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-01-27 10:01:36 -0600

    checking all of the employee ratings

--- client.js
+++ client.js
@@ -40,8 +40,42 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
+/* Live Solve */
+// iterate through the employees
+for (let i = 0; i < employees.length; i++) {
+  const employeeData = employees[i];
+
+  // call to my function and pass data
+  const result = processEmployeeBonus(employeeData);
+  console.log(result);
+}
+
 console.log(employees);
+
+function processEmployeeBonus(empData) {
+  // something happens here
+  let bonusPct = 0;
+
+  if (empData.reviewRating === 3) {
+    bonusPct = 4;
+  } else if (empData.reviewRating === 4) {
+    bonusPct = 6;
+  } else if (empData.reviewRating === 5) {
+    bonusPct = 10;
+  } else if (empData.reviewRating <= 2) {
+    bonusPct = 0;
+  }
+
+  return {
+    name: empData.name,
+    bonusPercentage: bonusPct,
+    totalCompensation: 0,
+    totalBonus: 0,
+  };
+}
 // employee in param will be object
+
+/* Bellow is where I was as a group
 function employeeBonusCalculator(employee) {
   // create object
   const newEmployeeObject = {
@@ -54,15 +88,14 @@ function employeeBonusCalculator(employee) {
   //loop through employee information
   for (let i = 0; i < employees.length; i++) {
     console.log(employees[i]);
-    /*
+
     if (employee.reviewRating <= 2) {
       //does not receive bonus
       console.log('reviewRating<=2');
     }
-    */
   }
 
   return; // return newEmployeeObject
-}
+} 
 
-console.log(employeeBonusCalculator(employees[3]));
+console.log(employeeBonusCalculator(employees[3])); */
